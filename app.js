88ef5304c014dc2de6d1c25082493c2eaf3d9751
David Park <dpark543107@gmail.com>
2016-04-20 15:49:42 -0500

    added cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors       = require('cors');
 
 require('./db/database');
 
@@ -19,6 +20,7 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
